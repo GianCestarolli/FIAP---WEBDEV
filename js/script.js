3368c42abeaab95dfd52343c264257e5211bcde7
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-03 22:45:28 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/*alert("Olá, mundo!")
+prompt("Digite seu nome")
+confirm("VOCÊ QUER MESMO SAIR?!?!?!!?!?")*/
+
+var nome = prompt("Qual seu nome?")
+var idade = prompt("Qual sua idade?")
+var profissão = prompt("Qual sua profissão?")
+
+console.log (`Olá ${nome}, você tem ${idade} anos e é um ${profissão}`)
+
