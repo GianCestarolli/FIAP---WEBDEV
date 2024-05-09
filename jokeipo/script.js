53effeb5580d4b631ada47b8890f236369cd3228
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-08 22:19:00 -0300

    first_commit

---
+++ jokeipo/script.js
@@ -0,0 +1,27 @@
+function escolhaComputador() {
+    const opcoes = ["Pedra", "Papel", "Tesoura"];
+    const indice = Math.floor(Math.random() * 3);
+    return opcoes[indice];
+}
+
+function jogar(escolhaJogador) {
+    const escolhaComp = escolhaComputador();
+    let resultado = "";
+
+    if (escolhaJogador === escolhaComp) {
+        resultado = "Empate!";
+        document.getElementById("resultado").style.color = "black"
+    } else if (
+        (escolhaJogador === "Pedra" && escolhaComp === "Tesoura") ||
+        (escolhaJogador === "Papel" && escolhaComp === "Pedra") ||
+        (escolhaJogador === "Tesoura" && escolhaComp === "Papel")
+    ) {
+        resultado = "Você ganhou!";
+        document.getElementById("resultado").style.color = "green";
+    } else {
+        resultado = "Computador ganhou!";
+        document.getElementById("resultado").style.color = "red";
+    }
+
+    document.getElementById("resultado").innerText = "Você escolheu: " + escolhaJogador + ". Computador escolheu: " + escolhaComp + ". " + resultado;
+}
